523d4a43b0a2f76711ffee788dd551d7ec0df40c
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-01-31 01:39:48 +0330

    add double tokens prediction

--- SelectLabelWords/LabelWordsExplorer.py
+++ SelectLabelWords/LabelWordsExplorer.py
@@ -65,6 +65,42 @@ class LabelWordsExplorer:
         top_tokens = self.tokenizer.convert_ids_to_tokens(top_indices[0])
         return [self.tokenizer.convert_tokens_to_string(top_token) for top_token in top_tokens]
 
+    def get_top_k_double_tokens(self, model_input):
+        # duplicate mask token
+        model_input = model_input.split(" ")
+        mask_index = model_input.index(self.mask)
+        model_input = model_input[:mask_index] + [self.mask] + model_input[mask_index:]
+        model_input = " ".join(model_input)
+        print(f"model input is {model_input}")
+
+        inputs = self.tokenizer(model_input, return_tensors="pt", padding="max_length", max_length=self.max_length,
+                                truncation=True)
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        with torch.no_grad():
+            outputs = self.language_model(**inputs)
+            logits = outputs.logits
+
+        mask_token_id = self.tokenizer.mask_token_id
+        mask_positions = (inputs == mask_token_id).nonzero(as_tuple=True)[1]
+
+        results = []
+        for pos in mask_positions:
+            mask_logits = logits[0, pos]  # (vocab_size,)
+            probs = torch.softmax(mask_logits, dim=-1)
+
+            top_probs, top_ids = torch.topk(probs, self.k2)
+            tokens = self.tokenizer.convert_ids_to_tokens(top_ids.tolist())
+            tokens = [self.tokenizer.convert_tokens_to_string(top_token) for top_token in tokens]
+            results.append(list(zip(tokens, top_probs.tolist())))
+
+        first_mask_token_prob, second_mask_token_prob = results[0], results[1]
+        double_token_probs = {}
+        for first_mask_token, first_mask_prob in first_mask_token_prob:
+            for second_mask_token, second_mask_prob in second_mask_token_prob:
+                double_token_probs[f"{first_mask_token}-{second_mask_token}"] = first_mask_prob * second_mask_prob
+        double_token_probs = dict(sorted(double_token_probs.items(), key=lambda x: x[1], reverse=True))
+        return list(double_token_probs.keys())[:self.k2]
+
     def find_label_words_single_token(self):
         result = {k: {} for k, _ in self.initial_label_words.items()}
         #print(f"result is {result}")
@@ -73,7 +109,7 @@ class LabelWordsExplorer:
             #print(f"selected demo is {demonstrations}")
             model_input = self.make_input(sample, demonstrations)
             print(f"model input is {model_input}")
-            top_k_tokens = self.get_top_k_tokens(model_input)
+            top_k_tokens = self.get_top_k_double_tokens(model_input)
             #print(f"top k tokens is {top_k_tokens}")
             for index, top_k_token in enumerate(top_k_tokens):
                 try:
