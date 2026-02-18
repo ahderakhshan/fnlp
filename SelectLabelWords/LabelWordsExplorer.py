e54c0680fe89791db33bf3d9d415d2dc138c3e9d
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-02-18 16:07:28 +0330

    fix input construction in double mask with punct

--- SelectLabelWords/LabelWordsExplorer.py
+++ SelectLabelWords/LabelWordsExplorer.py
@@ -73,12 +73,12 @@ class LabelWordsExplorer:
     def get_top_k_double_tokens(self, model_input):
         # duplicate mask token
         temp_model_input = model_input
+        model_input = model_input.split(" ")
         punctuation_index = temp_model_input.index(">") + 1
         if len(temp_model_input) != punctuation_index and temp_model_input[punctuation_index] != " ":
-            mask_index = temp_model_input.index(self.mask + temp_model_input[punctuation_index])
+            mask_index = model_input.index(self.mask + temp_model_input[punctuation_index])
         else:
-            mask_index = temp_model_input.index(self.mask)
-        model_input = model_input.split(" ")
+            mask_index = model_input.index(self.mask)
         model_input = " ".join(model_input[:mask_index]) + self.mask + " " + " ".join(model_input[mask_index:])
         print(f"model input {model_input}")
         inputs = self.tokenizer(model_input, return_tensors="pt", padding="max_length", max_length=self.max_length,
