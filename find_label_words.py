dcf2dfd70058500b183785b37f099076282ddc85
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-02-18 22:00:59 +0330

    fix

--- find_label_words.py
+++ find_label_words.py
@@ -87,7 +87,7 @@ elif args.task == "digikala-tc":
     KPTTemplatePath = "./templates/digikala-text-classification/templates_kpt.txt"
     LabelWordPath = "./labelwords/digikala-text-classification/labelwords.txt"
     LanguageModel = args.model_name_or_path
-    OutputPath = "./labelwords/digikalal-text-classification/explored_labelwords.txt"
+    OutputPath = "./labelwords/digikala-text-classification/explored_labelwords.txt"
     #main_label_words = {"Positive": "خوب", "Negative": "بد", "Neutral": "متوسط"}
     main_label_words = {"علم و تکنولوژی": "فناوری", "بازی ویدیویی": "بازی", "هنر و سینما": "سینما", "سلامت و زیبایی": "سلامتی","کتاب و ادبیات":"ادبیات", "راهنمای خرید": "خرید", "عمومی": "عمومی"}
     del_a_chars = [False, True]
